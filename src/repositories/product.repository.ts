84486b744fe91fac857d8cc72b61ce94afa837a5
dainamnguyen3@gmail.com <dainamnguyen3@gmail.com>
2026-01-01 15:17:37 +0700

    #4 config cors and DB

--- src/repositories/product.repository.ts
+++ src/repositories/product.repository.ts
@@ -2,6 +2,14 @@ import { prisma } from '../lib/prisma'
 
 export const productRepository = {
   findAll: async () => {
-    return await prisma.products.findMany()
+    return await prisma.products.findMany({
+      include: {
+        ProductImages: {
+          where: {
+            IsPrimary: true
+          }
+        }
+      }
+    })
   }
 }
