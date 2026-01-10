9f9930439e87c9bf8f1a701ecf8da941307e9911
dainamnguyen3@gmail.com <dainamnguyen3@gmail.com>
2026-01-10 16:11:03 +0700

    #17 config

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,6 +1,7 @@
 // viết hàm validate nhận vào checkSchema
 // và trả ra middleware xử lí lỗi
 
+import { EnityError, ErrorWithStatus } from '~/model/Errors'
 import { NextFunction, Request, Response } from 'express'
 import { ValidationChain, validationResult } from 'express-validator'
 import { RunnableValidationChains } from 'express-validator/lib/middlewares/schema'
@@ -14,7 +15,16 @@ export const validate = (validation: RunnableValidationChains<ValidationChain>) 
     if (errors.isEmpty()) {
       return next()
     } else {
-      res.status(HTTP_STATUS.UNPROCESSABLE_ENTITY).json({ message: ' Validation Failed', errors: errors.mapped() })
+      const errorObject = errors.mapped()
+      const entityError = new EnityError({ errors: {} })
+      for (const key in errorObject) {
+        const { msg } = errorObject[key]
+        if (msg instanceof ErrorWithStatus && msg.status !== HTTP_STATUS.UNPROCESSABLE_ENTITY) {
+          return next(msg)
+        }
+        entityError.errors[key] = msg
+      }
+      next(entityError)
     }
   }
 }
